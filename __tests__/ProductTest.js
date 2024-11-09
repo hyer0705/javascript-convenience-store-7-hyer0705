f5f154f30842f229d8b65e78d3dfeeb76030e7c7
조혜린 <hyer1205@gmail.com>
2024-11-09 12:02:22 +0900

    Feat(product): 상품 객체의 toString() 메서드 구현

--- __tests__/ProductTest.js
+++ __tests__/ProductTest.js
@@ -1,12 +1,16 @@
+import Product from '../src/models/Product.js';
+
 describe('상품 테스트', () => {
-  test('파일에 있는 상품으로 상품 객체 생성 및 toString()', async () => {
+  test.each([
+    ['콜라', 1000, 10, 10, '탄산2+1', '- 콜라 1,000원 10개 탄산2+1\n- 콜라 1,000원 10개\n'],
+    ['오렌지주스', 1800, 0, 9, 'MD추천상품', '- 오렌지주스 1,800원 9개 MD추천상품\n- 오렌지주스 1,800원 재고 없음\n'],
+    ['물', 500, 10, 0, null, '- 물 500원 10개\n'],
+  ])('상품이 %s, %i, %i, %i, %s 인 경우 toString() 호출한 경우', (name, price, generalQuantity, promotionQuantity, promotion, expected) => {
     // given
-    const product = new Product(콜라, 1000, 10, 10, '탄산2+1');
+    const product = new Product(name, price, generalQuantity, promotionQuantity, promotion);
 
     // when
-    const output = product.toString();
-
     // then
-    expect(output).toBe('- 콜라 1,000원 10개 탄산2+1\n- 콜라 1,000원 10개');
+    expect(product.toString()).toBe(expected);
   });
 });
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,13 +1,32 @@
 class Product {
-  constructor([name, price, quantity, promotion]) {
+  name;
+
+  price;
+
+  generalQuantity;
+
+  promotionQuantity;
+
+  promotion;
+
+  constructor(name, price, generalQuantity, promotionQuantity, promotion) {
     this.name = name;
     this.price = price;
-    this.quantity = quantity;
+    this.generalQuantity = generalQuantity;
+    this.promotionQuantity = promotionQuantity;
     this.promotion = promotion;
   }
 
   toString() {
-    return `- ${this.name} ${Number(this.price).toLocaleString('ko-KR')}원 ${this.quantity}개 ${this.promotion}`;
+    if (this.generalQuantity === 0 && this.promotionQuantity > 0) {
+      return `- ${this.name} ${this.price.toLocaleString('ko-KR')}원 ${this.promotionQuantity}개 ${this.promotion}\n- ${this.name} ${this.price.toLocaleString('ko-KR')}원 재고 없음\n`;
+    }
+
+    if (this.promotionQuantity === 0 && this.generalQuantity > 0) {
+      return `- ${this.name} ${this.price.toLocaleString('ko-KR')}원 ${this.generalQuantity}개\n`;
+    }
+
+    return `- ${this.name} ${this.price.toLocaleString('ko-KR')}원 ${this.promotionQuantity}개 ${this.promotion}\n- ${this.name} ${this.price.toLocaleString('ko-KR')}원 ${this.generalQuantity}개\n`;
   }
 }
 
